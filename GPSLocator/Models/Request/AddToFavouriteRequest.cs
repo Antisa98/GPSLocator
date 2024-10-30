7dc383ffca5dc04765596ee06cbe11d9291e9667
ANTISA\boian <boi.antisa@gmail.com>
2024-10-30 13:06:34 +0100

    no message

---
+++ GPSLocator/Models/Request/AddToFavouriteRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GPSLocator.Models.Request
+{
+	public record AddToFavouriteRequest
+	{
+		public int UserId { get; set; }
+
+		[JsonPropertyName("fsq_id")]
+		public string FoursquareId { get; set; }
+	}
+}
